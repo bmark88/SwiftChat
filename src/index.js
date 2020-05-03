8c58927297b68e8371b877776e347f50b15e06b6
bmark88 <bradley.mark@hotmail.com>
2020-05-02 20:28:52 -0400

    Setup websocket connection, and test it works with ping message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('Ping!');
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
 });
